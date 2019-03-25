45793c444eccfd5d27172e9140bf7f40c06c5c09
收藏夹1区 <41465042+shoucangjia1qu@users.noreply.github.com>
2019-03-25 23:26:32 +0800

    半双工聊天程序
    还是有点问题，明天再看看。

---
+++ tsTclnt_singlechat.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 25 23:10:28 2019
+
+@author: ecupl
+"""
+
+from socket import *
+
+HOST = 'localhost'
+PORT = 5677
+BUFSIZ = 1024
+ADDR = (HOST, PORT)
+
+tcpCliSock = socket(AF_INET, SOCK_STREAM)
+tcpCliSock.connect(ADDR)
+
+while True:
+    data = input('>')
+    if not data:
+        continue
+    tcpCliSock.send(bytes(data,'utf-8'))
+    data = tcpCliSock.recv(BUFSIZ)
+    print(data.decode('utf-8'))
+tcpCliSock.close()
+
+
+
+
+
+
+
---
+++ tsTserv_singlechat.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 25 22:59:08 2019
+
+@author: ecupl
+"""
+
+from socket import *
+
+HOST = ""
+PORT = 5677
+BUFSIZ = 1024
+ADDR = (HOST, PORT)
+
+#创建TCP套接字
+tcpSerSock = socket(AF_INET, SOCK_STREAM)
+tcpSerSock.bind(ADDR)
+tcpSerSock.listen(5)
+
+#接受服务端消息并处理
+while True:
+    print('waiting for connection...')
+    tcpCliSock, addr = tcpSerSock.accept()
+    print('connect from %s',addr)
+    while True:
+        data = tcpCliSock.recv(BUFSIZ)
+        data = data.decode('utf-8')
+        if data == 'quit':
+            tcpCliSock.close()
+        else:
+            print('%s said %s'%(addr,data))
+        senddata = ''
+        if senddata == '':
+            senddata = input('>')
+        tcpCliSock.send(bytes(senddata,'utf-8'))
+        data = None
+tcpSerSock.close()
+
+
+
+
+
+
+
+
+
